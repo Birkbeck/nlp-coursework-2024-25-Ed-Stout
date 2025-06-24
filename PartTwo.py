7ef49d4b558e8627fb49c3d99ef9e34b18fd3165
Ed Stout <ed.stout96@googlemail.com>
2025-06-24 19:59:58 +0100

    Checked that it works

--- PartTwo.py
+++ PartTwo.py
@@ -1,41 +1,40 @@
 import pandas as pd
 from pathlib import Path
 
-def preprocess_speeches(csv_path=None):
-    """
-    Reads the hansard40000.csv dataset and create df
-    """
-    csv_path = Path.cwd() / "texts" / "hansard40000.csv"
+csv_path = Path.cwd() / "p2-texts" / "hansard40000.csv"
 
-    df = pd.read_csv(csv_path)
+df = pd.read_csv(csv_path)
 
-    df['party'] = df['party'].replace('Labour (Co-op)', 'Labour') #1
+df['party'] = df['party'].replace('Labour (Co-op)', 'Labour') #1
 
-    party_counts = df['party'].value_counts() #2a
-    print("Counts per party before filtering:")
-    print(party_counts)
+df = df[df['party'] != 'Speaker'] #2c
 
-    sorted_counts = party_counts.sort_values(ascending=False) #2b
-    print("\nSorted counts (highest to lowest):")
-    print(sorted_counts)
+party_counts = df['party'].value_counts() #2a
+print("Counts per party before filtering:")
+print(party_counts)
 
-    top_parties = list(sorted_counts.index[:4]) #2c
-    print(f"\nTop 4 parties: {top_parties}")
+sorted_counts = party_counts.sort_values(ascending=False) #2b
+print("\nSorted counts (highest to lowest):")
+print(sorted_counts)
 
-    df = df[df['party'].isin(top_parties)] #2d
+top_parties = list(sorted_counts.index[:4]) #2c
+print(f"\nTop 4 parties: {top_parties}")
 
-    df = df.drop(columns=['Speaker']) #3
+#df = df[df['party'].isin(top_parties)] #2d
 
-    df = df[df['speech_class'] == 'Speech'] #4
+#df = df[df['speech_class'] == 'Speech'] #3
 
-    df = df[df['speech'].str.len() >= 1000] #5
-    
-    """cleaned_rows = []
-    for _, row in df.iterrows(): #4
-        is_speech = (row['speech_class'] == 'Speech')
-        long_enough = (len(row['speech']) >= 1000)
-        if is_speech and long_enough:
-            cleaned_rows.append(row)"""
+#df = df[df['speech'].str.len() >= 1000] #4
 
-    cleaned_df = pd.DataFrame(cleaned_rows).reset_index(drop=True)
-    return cleaned_df
+"""cleaned_rows = []
+for _, row in df.iterrows(): #4
+    is_speech = (row['speech_class'] == 'Speech')
+    long_enough = (len(row['speech']) >= 1000)
+    if is_speech and long_enough:
+        cleaned_rows.append(row)"""
+
+#cleaned_df = pd.DataFrame(cleaned_rows).reset_index(drop=True)
+#return cleaned_df
+
+new_party_counts = df['party'].value_counts().sort_values(ascending=False) 
+print(new_party_counts)
