d17177c4cc6e93db86596e9f76e9ebe102aa396a
Ed Stout <ed.stout96@googlemail.com>
2025-06-24 21:19:47 +0100

    Adding TfidfVectorizer from scikit-learn so they are ready for further analysis

--- PartTwo.py
+++ PartTwo.py
@@ -1,29 +1,27 @@
 import pandas as pd
 from pathlib import Path
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 
+#Question i
 csv_path = Path.cwd() / "p2-texts" / "hansard40000.csv"
 
 df = pd.read_csv(csv_path)
 
 df['party'] = df['party'].replace('Labour (Co-op)', 'Labour') #1
-
 df = df[df['party'] != 'Speaker'] #2c
 
 party_counts = df['party'].value_counts() #2a
-print("Counts per party before filtering:")
-print(party_counts)
+#print(party_counts)
 
 sorted_counts = party_counts.sort_values(ascending=False) #2b
-print("\nSorted counts (highest to lowest):")
-print(sorted_counts)
+#print(sorted_counts)
 
 top_parties = list(sorted_counts.index[:4]) #2c
 print(f"\nTop 4 parties: {top_parties}")
 
 df = df[df['party'].isin(top_parties)] #2d
-
 df = df[df['speech_class'] == 'Speech'] #3
-
 df = df[df['speech'].str.len() >= 1000] #4
 
 """cleaned_rows = []
@@ -31,12 +29,16 @@ for _, row in df.iterrows(): #4
     is_speech = (row['speech_class'] == 'Speech')
     long_enough = (len(row['speech']) >= 1000)
     if is_speech and long_enough:
-        cleaned_rows.append(row)"""
-
-#cleaned_df = pd.DataFrame(cleaned_rows).reset_index(drop=True)
-#return cleaned_df
+        cleaned_rows.append(row)
+cleaned_df = pd.DataFrame(cleaned_rows).reset_index(drop=True)
+return cleaned_df"""
 
 #new_party_counts = df['party'].value_counts().sort_values(ascending=False) 
 #print(new_party_counts)
-print(df.shape)
+#print(df.shape)
+
+#Question ii
+vectorizer = TfidfVectorizer(stop_words='english', max_features=3000)
+X = vectorizer.fit_transform(df['speech'])
+y = df['party']
 
